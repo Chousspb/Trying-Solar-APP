9dc7b1b20dee264ae3aab0ab8c0b03f615a856d1
Chousspb <chous_spb@mail.ru>
2023-03-30 22:14:01 +0300

    Initial Commit

---
+++ Trying Solar APP/Trying_Solar_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trying_Solar_APPApp.swift
+//  Trying Solar APP
+//
+//  Created by Admin on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Trying_Solar_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
